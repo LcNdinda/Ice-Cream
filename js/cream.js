1ef71b496fa7dedd567f0dae7af73bd68b220149
tarick ali <tarickaliabdi@gmail.com>
2018-08-31 09:43:17 +0300

    added the functions of the footer

--- js/cream.js
+++ js/cream.js
@@ -1,1 +1,30 @@
+function resetFields(){
+  $("input#name").val("");
+  $("textarea#comments").val("");
 
+}
+
+$(document).ready(function(){
+  $("form#feedbacks").submit(function(event){
+    event.preventDefault;
+    var inputtedName = $("input#name").val();
+    var inputComment = $("textarea#comments").val();
+
+    resetFields();
+
+  });
+  $("li.footer1").click(function(){
+    $("#footer").fadeIn(slow);
+  })
+});
+
+$(window).scroll(function() {
+  $(".slideanim").each(function(){
+    var pos = $(this).offset().top;
+
+    var winTop = $(window).scrollTop();
+    if (pos < winTop + 600) {
+      $(this).addClass("slide");
+    }
+  });
+});
